ed82b1b8d6bc164c8502c25de5080f71b935d8d7
Facundo <fandinofacundo123>
2024-10-10 19:16:17 -0300

    Descargar CV agregado

--- components/Button/page.jsx
+++ components/Button/page.jsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 import "./style.scss";
 
 export default function Button(props){
+    const archivo = "/archivos/cvActualizadoNuevo.pdf";
     const [hovered, setHovered] = useState(props.back);
     const [color, setColor] = useState("black")
 
@@ -18,17 +19,36 @@ export default function Button(props){
     }
 
     return(
-        <Link href={props.link} style={{zIndex: 2, textDecoration: "none"}}>
-        <div
-        className="buttonContainer" 
-        style={{backgroundColor: hovered}}
-        onMouseOver={handleOver}
-        onMouseOut={handleOut}
-        >
+        <>
+        {props.content == "Descargar CV" ? (
+            <a 
+            href={archivo} 
+            download="CV_Facundo_Fandiño"
+            style={{textDecoration:"none"}}>
+                <div
+                className="buttonContainer" 
+                style={{backgroundColor: hovered}}
+                onMouseOver={handleOver}
+                onMouseOut={handleOut}
+                >
+                    <p className="content" style={{color: color}}>{props.content}</p>
+                </div>
+            </a>
+        ):
+            <Link 
+            href={props.link} 
+            style={{textDecoration: "none"}}>
+                <div
+                className="buttonContainer" 
+                style={{backgroundColor: hovered}}
+                onMouseOver={handleOver}
+                onMouseOut={handleOut}
+                >
+                    <p className="content" style={{color: color}}>{props.content}</p>
+                </div>
+            </Link>
         
-        <p className="content" style={{color: color}}>{props.content}</p>
-
-        </div>
-        </Link>
+        }
+        </>
     )
 }
